181f7c2c5475e76800292a8e44336ba05ab90c9e
bilal simsek <xyzbilal@gmail.com>
2023-07-25 10:15:22 +0300

    Initial Commit

---
+++ RadialLayout/RadialLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadialLayoutApp.swift
+//  RadialLayout
+//
+//  Created by Bilal SIMSEK on 25.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct RadialLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
